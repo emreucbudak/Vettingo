8659d21ecb67cbeed4481eb1f56a0dc323088ef1
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-08-14 18:06:47 +0300

    Add CORS configuration to gateway

--- src/Gateway/Vettingo.Gateway.API/Program.cs
+++ src/Gateway/Vettingo.Gateway.API/Program.cs
@@ -1,7 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var origins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins(origins)
+        .AllowAnyHeader()
+        .AllowCredentials() 
+        .AllowAnyMethod();
+    });
+});
 builder.Services.AddControllers();
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
@@ -9,7 +19,7 @@ var app = builder.Build();
 
 app.MapReverseProxy();
 app.UseHttpsRedirection();
-
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
