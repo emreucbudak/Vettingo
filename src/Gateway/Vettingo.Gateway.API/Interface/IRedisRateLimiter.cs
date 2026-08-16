d0dbbf40abbd4614e9a1c55f40e8cf2659f7dbae
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-08-16 17:01:29 +0300

    Add Redis-backed gateway rate limiting

--- src/Gateway/Vettingo.Gateway.API/Interface/IRedisRateLimiter.cs
+++ src/Gateway/Vettingo.Gateway.API/Interface/IRedisRateLimiter.cs
@@ -2,6 +2,6 @@
 {
     public interface IRedisRateLimiter
     {
-        Task<bool> CheckRateLimit();
+        Task<bool> CheckRateLimit(string key);
     }
 }
---
+++ src/Gateway/Vettingo.Gateway.API/Middleware/RedisRateLimitMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Vettingo.Gateway.API.Interface;
+
+namespace Vettingo.Gateway.API.Middleware
+{
+    public  class RedisRateLimitMiddleware(IRedisRateLimiter redisRateLimiter) : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var email = context.User?.FindFirst(c => c.Type == "email")?.Value;
+            if(email is null)
+            {
+                throw new Exception("İstediğiniz işlem için giriş yapmalısınız.");
+            }
+            bool check = await redisRateLimiter.CheckRateLimit(email);
+            if (!check)
+            {
+                context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                await context.Response.WriteAsync("Çok fazla istek gönderdiniz. Lütfen daha sonra tekrar deneyin.");
+            }
+            await next(context);
+
+        }
+    }
+}
