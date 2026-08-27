6fc5a2712db27fe153cdfbdd52b6e0bafafa075f
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-08-27 18:35:59 +0300

    Add AuthService Identity registration

--- src/AuthService/Vettingo.AuthService.Persistence/Registration/DbContextRegistration.cs
+++ src/AuthService/Vettingo.AuthService.Persistence/Registration/DbContextRegistration.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Vettingo.AuthService.Application.Repository;
+using Vettingo.AuthService.Domain.Entities;
 using Vettingo.AuthService.Persistence.Repository;
 
 namespace Vettingo.AuthService.Persistence.Registration
@@ -14,6 +15,11 @@ namespace Vettingo.AuthService.Persistence.Registration
             {
                 options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
             });
+
+            collect
+                .AddIdentityCore<User>()
+                .AddRoles<Role>()
+                .AddEntityFrameworkStores<DbContext.IdentityDbContext>();
 
             collect.AddScoped<ICompanyRepository, CompanyRepository>();
         }
